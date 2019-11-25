b9314163e73ba3d07406e753eb80fe9975360276
willywax <williamtrans2011@gmail.com>
2019-11-25 08:25:34 +0200

    Updated links

--- src/components/courses/CourseList.js
+++ src/components/courses/CourseList.js
@@ -1,46 +1,46 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import Proptypes from 'prop-types';
+import Proptypes from "prop-types";
 
 const CourseList = ({ courses }) => (
-    <table className="table">
-        <thead>
-            <tr>
-                <th>Watch</th>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Category</th>
-            </tr>
-        </thead>
-        <tbody>
-            {courses.map(course => {
-                return (
-                    <tr key={course.slug}>
-                        <td>
-                            <a className='btn btn-light'
-                                href={'https://www.pluralsight.com/courses/react-redux-react-router-es6' + course.slug}
-                            >
-                                Watch
-                        </a>
-                        </td>
-                        <td>
-                            <Link to={"/course/" + course.slug}>{course.title}</Link>
-                        </td>
-                        <td>
-                            {course.authorName}
-                        </td>
-                        <td>
-                            {course.category}
-                        </td>
-                    </tr>
-                )
-            })}
-        </tbody>
-    </table>
+  <table className="table">
+    <thead>
+      <tr>
+        <th>Watch</th>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Category</th>
+      </tr>
+    </thead>
+    <tbody>
+      {courses.map(course => {
+        return (
+          <tr key={course.slug}>
+            <td>
+              <a
+                className="btn btn-light"
+                href={
+                  "https://www.pluralsight.com/courses/react-redux-react-router-es6" +
+                  course.slug
+                }
+              >
+                Watch
+              </a>
+            </td>
+            <td>
+              <Link to={"/course/" + course.slug}>{course.title}</Link>
+            </td>
+            <td>{course.authorName}</td>
+            <td>{course.category}</td>
+          </tr>
+        );
+      })}
+    </tbody>
+  </table>
 );
 
 CourseList.propTypes = {
-    courses: Proptypes.array.isRequired
-}
+  courses: Proptypes.array.isRequired
+};
 
 export default CourseList;
